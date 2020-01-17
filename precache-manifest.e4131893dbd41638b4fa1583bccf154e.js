2f4783fb51e8fe3ed7c08947685a5d9600a89079
nguyenchiemhao <nguyenchiemhao2205@gmail.com>
2020-01-17 10:46:59 +0700

    Updates

---
+++ precache-manifest.e4131893dbd41638b4fa1583bccf154e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c98b41b611c46da232a5d483780273",
+    "url": "/Ecommerce-Clothing-Reactjs/index.html"
+  },
+  {
+    "revision": "3958e769f42394dbcb75",
+    "url": "/Ecommerce-Clothing-Reactjs/static/css/main.82ea051e.chunk.css"
+  },
+  {
+    "revision": "bb54929537fb0d8235a0",
+    "url": "/Ecommerce-Clothing-Reactjs/static/js/2.4d697a1e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Ecommerce-Clothing-Reactjs/static/js/2.4d697a1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3958e769f42394dbcb75",
+    "url": "/Ecommerce-Clothing-Reactjs/static/js/main.c217061b.chunk.js"
+  },
+  {
+    "revision": "d53acf2cfdc8801ff694",
+    "url": "/Ecommerce-Clothing-Reactjs/static/js/runtime-main.38ce738c.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/Ecommerce-Clothing-Reactjs/static/media/logo.19ef7b17.svg"
+  }
+]);
